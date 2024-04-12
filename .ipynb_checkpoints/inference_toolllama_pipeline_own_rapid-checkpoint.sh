347681cde928b55f1062586a1d9694f33b09aaf6
EduardoTrevino <realeddietrevino@gmail.com>
2024-04-12 15:42:59 -0600

    Added model file and modified inf bash

--- .ipynb_checkpoints/inference_toolllama_pipeline_own_rapid-checkpoint.sh
+++ .ipynb_checkpoints/inference_toolllama_pipeline_own_rapid-checkpoint.sh
@@ -1,6 +1,6 @@
 export CUDA_VISIBLE_DEVICES=0
-export RAPIDAPI_KEY="THE RAPID API KEY"
-export OUTPUT_DIR="output/toolllama-2_qaq_v.0.0.0.3"
+export RAPIDAPI_KEY=$(cat rapidapi.txt)
+export OUTPUT_DIR="outputs/toolllama-2_qaq_v.0.0.0.3"
 export PYTHONPATH=./
 
 mkdir $OUTPUT_DIR
