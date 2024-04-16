9748c3f7f2874a245be943a5f5362872c44efffc
EduardoTrevino <realeddietrevino@gmail.com>
2024-04-15 21:34:13 -0600

    Added G1 tool type 3 questions

--- clean_data.py
+++ clean_data.py
@@ -1,15 +1,15 @@
 import json
 
 # File paths
-input_filename = 'G1_instruction.json'
-output_filename = 'G1_instruction_quik.json'
+input_filename = 'G1_tool.json'
+output_filename = 'G1_tool_type3_no_qaq.json'
 
 # Load JSON data from a file
 with open(input_filename, 'r') as file:
     data = json.load(file)
 
 # IDs to keep
-ids_to_keep = {608, 2144, 3308, 3922, 6736, 10160}
+ids_to_keep = {692, 2513, 5695, 8129, 13495, 14198}
 
 # Filter the data to keep only entries with specified query_id
 filtered_data = [entry for entry in data if entry['query_id'] in ids_to_keep]
