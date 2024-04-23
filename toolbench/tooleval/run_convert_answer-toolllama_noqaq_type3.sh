43a8dd9f1807241a18cc44a0d6d93bc51cc0f480
Rohit Vinod Malhotra <rvmalhot@babel-login-3.lti.cs.cmu.edu>
2024-04-23 14:10:26 -0400

    creating converted outputs

--- toolbench/tooleval/run_convert_answer-toolllama_noqaq_type3.sh
+++ toolbench/tooleval/run_convert_answer-toolllama_noqaq_type3.sh
@@ -1,12 +1,19 @@
-export RAW_ANSWER_PATH=../../outputs/toolllama-2_no_qaq_G1_category_type3/
-export CONVERTED_ANSWER_PATH=../../model_predictions_converted/
-export MODEL_NAME=toolllama
-export METHOD=DFS
-mkdir ${CONVERTED_ANSWER_PATH}/${MODEL_NAME}
+export RAW_ANSWER_PATH=../../outputs
+export CONVERTED_ANSWER_PATH=../../model_predictions_converted
+export MODEL_NAME=GPT
+export METHOD=CoT
+mkdir -p ${CONVERTED_ANSWER_PATH}/${MODEL_NAME}
 
-for test_set in G1_category_no_qaq_type3
+
+for test_set in G1_category_type1_no_qaq_v2_chatgpt \
+                G1_category_type4_no_qaq_v2_chatgpt \
+                G1_instruction_type1_no_qaq_v2_chatgpt \
+                G1_instruction_type4_no_qaq_v2_chatgpt \
+                G1_tool_type1_no_qaq_v2_chatgpt \
+                G1_tool_type4_no_qaq_v2_chatgpt
+
 do
-    answer_dir=${RAW_ANSWER_PATH}/${MODEL_NAME}/${test_set}
+    answer_dir=${RAW_ANSWER_PATH}/${test_set}
     output_file=${CONVERTED_ANSWER_PATH}/${MODEL_NAME}/${test_set}.json
     
     python convert_to_answer_format.py\
--- toolbench/tooleval/run_pass_rate-toolllama_noqaq_type3.sh
+++ toolbench/tooleval/run_pass_rate-toolllama_noqaq_type3.sh
@@ -1,12 +1,12 @@
 export CONVERTED_ANSWER_PATH=../../model_predictions_converted/
-export SAVE_PATH=pass_rate_results_toolllama_no_qaq_type3_v2
-export CANDIDATE_MODEL=toolllama
+export SAVE_PATH=pass_rate_G1_chatgpt_cot
+export CANDIDATE_MODEL=chatgpt_cot
 export API_POOL_FILE=openai_key_json_file.json
 
 python eval_pass_rate.py \
     --converted_answer_path ${CONVERTED_ANSWER_PATH} \
     --save_path ${SAVE_PATH} \
     --reference_model ${CANDIDATE_MODEL} \
-    --test_ids ../../model_predictions_converted/toolllama/ \
+    --test_ids ../../model_predictions_converted/GPT/ \
     --max_eval_threads 20 \
     --evaluate_times 7
