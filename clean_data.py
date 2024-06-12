e2c7d6e5449d5506ddf2747d1aa3f86cb7a5687a
ET <realeddietrevino@gmail.com>
2024-06-12 13:58:02 -0500

    Created delete folder, moving files for EoA

--- clean_data.py
+++ clean_data.py
@@ -2,14 +2,20 @@ import json
 
 # File paths
 input_filename = 'G1_tool.json'
-output_filename = 'G1_tool_type3_no_qaq.json'
+input_filename = 'G1_category.json'
+input_filename = 'G1_instruction.json'
+input_filename = 'G2_category.json'
+input_filename = 'G2_instruction.json'
+input_filename = 'G3_instruction.json'
+
+output_filename = 'cleaned_toolbench.json'
 
 # Load JSON data from a file
 with open(input_filename, 'r') as file:
     data = json.load(file)
 
 # IDs to keep
-ids_to_keep = {692, 2513, 5695, 8129, 13495, 14198}
+ids_to_keep = {}
 
 # Filter the data to keep only entries with specified query_id
 filtered_data = [entry for entry in data if entry['query_id'] in ids_to_keep]
